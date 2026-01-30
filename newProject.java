29c825c3f193ff3454a2536190f1e3d65269f1cf
Chloe <cflirio@cpp.edu>
2026-01-30 09:41:44 -0800

    Initial Commit

---
+++ newProject.java
@@ -0,0 +1,9 @@
+//create a java program thats simple and has a bug in it
+public class NewProject {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0; // Bug: division by zero
+        int c = a / b; // This line will cause an error
+        System.out.println("Result: " + c);
+    }
+} 
