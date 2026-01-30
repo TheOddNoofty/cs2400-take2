fbd11a4bcaa728c62d6e796cd5d2e81cc1493800
Chloe <cflirio@cpp.edu>
2026-01-30 10:03:13 -0800

    Second commit!

--- newProject.java
+++ newProject.java
@@ -1,8 +1,8 @@
 //create a java program thats simple and has a bug in it
-public class NewProject {
+public class newProject {
     public static void main(String[] args) {
         int a = 10;
-        int b = 0; // Bug: division by zero
+        int b = 2; // Bug: division by zero
         int c = a / b; // This line will cause an error
         System.out.println("Result: " + c);
     }
