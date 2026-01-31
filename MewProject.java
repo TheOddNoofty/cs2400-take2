dff2cfbf026e150fc060d0f3735513c61b1079ea
Chloe <cflirio@cpp.edu>
2026-01-30 19:00:34 -0800

    "Commit this back for video"

--- MewProject.java
+++ MewProject.java
@@ -2,7 +2,7 @@
 public class MewProject{
     public static void main(String[] args) {
         int a = 10;
-        int b = 4; 
+        int b = 2; 
         int c = a / b; 
         System.out.println("Result: " + c);
     }
