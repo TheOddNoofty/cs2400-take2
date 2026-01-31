64ed4b34b86ef387fb756187dca4c6dc800bb182
Chloe <cflirio@cpp.edu>
2026-01-30 20:39:06 -0800

    Commit

--- MewProject.java
+++ MewProject.java
@@ -2,7 +2,7 @@
 public class MewProject{
     public static void main(String[] args) {
         int a = 10;
-        int b = 2; //demo comment 
+        int b = 5; 
         int c = a / b; 
         System.out.println("Result: " + c);
     }
