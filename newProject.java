df1debdc3f543550eb0a4033405bb5b9857bbb5c
Chloe <cflirio@cpp.edu>
2026-01-30 17:13:37 -0800

    "deleted unessasary project"

--- newProject.java
+++
@@ -1,9 +0,0 @@
-//create a java program thats simple and has a bug in it
-public class newProject {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 2; // Bug: division by zero
-        int c = a / b; // This line will cause an error
-        System.out.println("Result: " + c);
-    }
-} 
